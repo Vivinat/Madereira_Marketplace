f4a74149b28b0ed26dd755195f9306bf33c8deb7
João Prado <jpdiasprado01@gmail.com>
2024-10-15 20:31:37 -0300

    primeiro teste configurado

--- BackEnd/routes/tests/products.test.js
+++ BackEnd/routes/tests/products.test.js
@@ -5,7 +5,7 @@ describe("GET produtos/", () => {
     describe("rota deve retornar todos os produtos", () =>{
         // status code deve ser 200
         test("rota deve responder com status code 200", async () => {
-            const response = await request.request(app).get("/api/v1/produtos/")
+            const response = await request(app).get("/api/v1/produtos/")
             expect(response.statusCode).toBe(200)
         })
     })
